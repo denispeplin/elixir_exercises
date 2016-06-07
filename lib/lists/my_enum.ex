a9b30ec6582b3ba28e2ffc5abf44b222641e5a35
denispeplin <denis.peplin@gmail.com>
2016-06-07 22:07:07 +0300

    Implement MyEnum#take.

--- lib/lists/my_enum.ex
+++ lib/lists/my_enum.ex
@@ -103,4 +103,36 @@ defmodule Lists.MyEnum do
     split(list, new_count)
   end
   def split(list, _), do: {[], list}
+
+  @doc """
+  Takes the first `count` items from the enumerable.
+
+  `count` must be an integer. If a negative `count` is given, the last
+  `count` values will be taken.
+  For such, the enumerable is fully enumerated keeping up
+  to `2 * count` elements in memory. Once the end of the enumerable is
+  reached, the last `count` elements are returned.
+
+  ## Examples
+
+      iex> Lists.MyEnum.take([1, 2, 3], 2)
+      [1, 2]
+
+      iex> Lists.MyEnum.take([1, 2, 3], 10)
+      [1, 2, 3]
+
+      iex> Lists.MyEnum.take([1, 2, 3], 0)
+      []
+
+      iex> Lists.MyEnum.take([1, 2, 3], -2)
+      [2, 3]
+  """
+  def take([head | tail], count) when count > 0 do
+    [head | take(tail, count - 1)]
+  end
+  def take(list, count) when count < 0 do
+    {_, right} = split(list, count)
+    right
+  end
+  def take(_, _), do: []
 end
